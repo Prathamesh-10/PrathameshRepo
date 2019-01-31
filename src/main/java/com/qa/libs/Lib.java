194efa6fcf3d8bdcaab768c0b492411194cfca45
Prathamesh Hule <PrathameshH@citiustech.com>
2019-01-31 18:01:14 +0530

    First Git Commit for my selenium code

---
+++ src/main/java/com/qa/libs/Lib.java
@@ -0,0 +1,9 @@
+package com.qa.libs;
+
+public class Lib {
+
+	public void test()
+	{
+		System.out.println("test_Method");
+	}
+}
---
+++ src/main/java/com/qa/utils/Utility.java
@@ -0,0 +1,9 @@
+package com.qa.utils;
+
+public class Utility {
+
+	public void test_Util()
+	{
+		System.out.println("Utility Test");
+	}
+}
