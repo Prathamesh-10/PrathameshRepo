5d7119675aaa7109043fe81022d5986536b6341f
Prathamesh Hule <PrathameshH@citiustech.com>
2019-02-01 12:19:21 +0530

    Added stash page

---
+++ src/main/java/com/qa/pages/StashPage.java
@@ -0,0 +1,11 @@
+package com.qa.pages;
+
+public class StashPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is stash method");
+	}
+
+}
